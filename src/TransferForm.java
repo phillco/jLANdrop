1bd9842608806c603d9c5bfd25169eb6d9d1990a
Phillip Cohen <pc@phillipcohen.net>
2010-05-11 19:37:00 +0000

    Redid the TransferForm's layout; it's now much more resilient (read: correct).

--- src/TransferForm.java
+++ src/TransferForm.java
@@ -32,32 +32,33 @@ public class TransferForm extends JFrame
 		detailLabel1 = new JLabel();
 		detailLabel2 = new JLabel();
 
-		detailLabel1.setPreferredSize( new Dimension( 350, detailLabel1.getHeight() ) );
 		detailLabel1.setAlignmentX( CENTER_ALIGNMENT );
-		detailLabel2.setPreferredSize( new Dimension( 350, detailLabel2.getHeight() ) );
 		detailLabel2.setAlignmentX( CENTER_ALIGNMENT );
 		detailLabel2.setForeground( SystemColor.controlDkShadow );
 
-		add( Box.createRigidArea( new Dimension( 5, 8 ) ) );
+		add( Box.createVerticalStrut( 5 ));
 		add( statusLabel );
-		add( Box.createRigidArea( new Dimension( 5, 5 ) ) );
+		add( Box.createVerticalStrut( 5 ));
 		add( detailLabel1 );
-		add( Box.createRigidArea( new Dimension( 5, 3 ) ) );
+		add( Box.createVerticalStrut( 3 ));
 		add( detailLabel2 );
-		add( Box.createRigidArea( new Dimension( 5, 8 ) ) );
-
+		add( Box.createVerticalStrut( 8 ));
+		add( Box.createVerticalGlue());
+		
 		JPanel progressPanel = new JPanel();
 		{
 			progressBar = new JProgressBar();
-			progressPanel.add( progressBar );
+			progressPanel.add( progressBar );		
 			progressBar.setPreferredSize( new Dimension( 300, 25 ) );
 			progressPanel.setPreferredSize( new Dimension( 250, 65 ) );
 		}
 		add( progressPanel );
-		add( Box.createRigidArea( new Dimension( 5, 15 ) ) );
+		add( Box.createVerticalStrut( 3 ));
+		add( Box.createVerticalGlue());
+
 
 		updateComponents();
-		setSize( 350, 145 );
+		setSize( 350, 155 );
 		setResizable( false );
 
 		addWindowListener( new java.awt.event.WindowAdapter()
@@ -78,10 +79,10 @@ public class TransferForm extends JFrame
 
 	public void updateComponents()
 	{
-		setTitle( transfer.getName() );
-		progressBar.setValue( transfer.getProgress() );
 		if ( ( transfer.getStage() != Transfer.Stage.TRANSFERRING ) || ( System.currentTimeMillis() - lastUpdateTime > 100 ) )
 		{
+			setTitle( transfer.getName() );
+			progressBar.setValue( transfer.getProgress() );
 			statusLabel.setText( transfer.toString() );
 			detailLabel1.setText( transfer.getDetails() );
 			detailLabel2.setText( transfer.getTimeLeft() );
