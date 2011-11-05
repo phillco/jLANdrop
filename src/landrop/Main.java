2202810e0b2b60d4fdc0cd5616ae2545aae8d7b1
Phillip Cohen <pc@phillipcohen.net>
2011-11-04 22:21:37 -0500

    Version bump to v1.5.

--- src/landrop/Main.java
+++ src/landrop/Main.java
@@ -6,6 +6,8 @@ import landrop.ui.DropForm;
 
 public class Main
 {
+    public static final String VERSION = "v1.5";
+
 	private static DropForm mainFrame;
 
 	private static Listener listener = new Listener();
